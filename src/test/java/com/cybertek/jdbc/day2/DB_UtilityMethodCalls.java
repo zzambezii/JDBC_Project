fd5a6b63bac02639a97810e73a816476babf0736
Akbar <akbar@cybertekschool.com>
2020-08-04 17:10:56 -0400

    JDBC Day 2 : Creating Utility Class with Reusable methods

---
+++ src/test/java/com/cybertek/jdbc/day2/DB_UtilityMethodCalls.java
@@ -0,0 +1,33 @@
+package com.cybertek.jdbc.day2;
+
+import sun.security.pkcs11.Secmod;
+
+public class DB_UtilityMethodCalls {
+
+    public static void main(String[] args) {
+
+        DB_Utility.createConnection();
+        DB_Utility.runQuery("SELECT * FROM JOBS");
+
+//        DB_Utility.displayAllData();
+
+//        System.out.println("Column count is " + DB_Utility.getColumnCNT() );
+//        System.out.println("Row count is "    + DB_Utility.getRowCount() );
+//
+//        System.out.println("Getting 3rd row of Jobs Table \n "
+//                                + DB_Utility.getRowDataAsList(3) );
+//        System.out.println("Getting 2nd Column as a list \n"
+//                                  + DB_Utility.getColumnDataAsList(2) );
+//        System.out.println("Getting JOB_TITLE Column as a list \n"
+//                                  + DB_Utility.getColumnDataAsList("JOB_TITLE") );
+//        System.out.println("Getting 3rd row , 2nd column data \n"
+//                                    + DB_Utility.getColumnDataAtRow(3,2) );
+//        System.out.println("Getting 3rd row , JOB_TITLE column data \n"
+//                                    + DB_Utility.getColumnDataAtRow(3,"JOB_TITLE") );
+//        System.out.println("Getter the data at 3rd row as Map : \n"
+//                                    + DB_Utility.getRowMap(3) );
+        DB_Utility.destroy();
+
+    }
+
+}
