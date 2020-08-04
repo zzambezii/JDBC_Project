6aa90c32eedfe538a209ce6b50c88a062549802f
Akbar <akbar@cybertekschool.com>
2020-08-04 14:12:34 -0400

    getColumnDataAtRow method starter

--- src/test/java/com/cybertek/jdbc/day2/DB_Utility.java
+++ src/test/java/com/cybertek/jdbc/day2/DB_Utility.java
@@ -4,64 +4,83 @@ import java.sql.*;
 
 public class DB_Utility {
     // adding static field so we can access in all static methods
-     private static Connection conn ;
-     private static  ResultSet rs ;
+    private static Connection conn;
+    private static ResultSet rs;
 
-     /*
+
+    /*
+    * Getting single column cell value at certain row
+    * row 2 column 3  -->> the data
+    * */
+
+    /**
+     * Getting single column cell value at certain row
+     * @param rowNum    row number we want to get data from
+     * @param columnIndex  column index we want to get the data from
+     * @return the data in String
+     */
+    public static String getColumnDataAtRow (int rowNum , int columnIndex){
+
+
+
+    }
+
+
+
+
+    /*
      * a method to display all the data in the result set
      *
      * */
-     public static void displayAllData(){
+    public static void displayAllData() {
 
-         // get the first row data  | without knowing the column names
-         int colCount = DB_Utility.getColumnCNT() ;
-         // in order to get whole result cursor must be at before first location !
+        // get the first row data  | without knowing the column names
+        int colCount = DB_Utility.getColumnCNT();
+        // in order to get whole result cursor must be at before first location !
 
-         try {
-             // in order to start from beginning , we should be at beforefirst location
-             rs.beforeFirst(); // this is for below loop to work
-             while (rs.next() == true) { // row iteration
+        try {
+            // in order to start from beginning , we should be at beforefirst location
+            rs.beforeFirst(); // this is for below loop to work
+            while (rs.next() == true) { // row iteration
 
-                 for (int i = 1; i <= colCount; i++) { // column iteration
-                     System.out.print(rs.getString(i) + "\t");
-                 }
-                 System.out.println(); /// adding a blank line for next line
-             }
-             // now the cursor is at after last location
-             // move it back to before first location so we can have no issue calling the method again
-             rs.beforeFirst(); // this is for next method that might need to be at before first location
+                for (int i = 1; i <= colCount; i++) { // column iteration
+                    System.out.print(rs.getString(i) + "\t");
+                }
+                System.out.println(); /// adding a blank line for next line
+            }
+            // now the cursor is at after last location
+            // move it back to before first location so we can have no issue calling the method again
+            rs.beforeFirst(); // this is for next method that might need to be at before first location
 
-         }catch(SQLException e){
-             System.out.println("ERROR WHILE GETTING ALL DATA");
-             e.printStackTrace();
-         }
+        } catch (SQLException e) {
+            System.out.println("ERROR WHILE GETTING ALL DATA");
+            e.printStackTrace();
+        }
 
-     }
+    }
 
 
-
-     /*
+    /*
      * a method to get the column count of the current ResultSet
      *
      *   getColumnCNT()
      *
      * */
-     public static int getColumnCNT(){
+    public static int getColumnCNT() {
 
-         int colCount = 0  ;
+        int colCount = 0;
 
-         try {
-             ResultSetMetaData rsmd = rs.getMetaData();
-             colCount = rsmd.getColumnCount() ;
+        try {
+            ResultSetMetaData rsmd = rs.getMetaData();
+            colCount = rsmd.getColumnCount();
 
-         } catch (SQLException e) {
-             System.out.println("ERROR WHILE COUNTING THE COLUMNS");
-             e.printStackTrace();
-         }
+        } catch (SQLException e) {
+            System.out.println("ERROR WHILE COUNTING THE COLUMNS");
+            e.printStackTrace();
+        }
 
-        return colCount ;
-     }
-
+        return colCount;
+    }
 
 
     /*
@@ -84,27 +103,26 @@ public class DB_Utility {
 
     }
 
-     /*
+    /*
      * a static method to get the ResultSet object
      * with valid connection by executing query
      * */
-    public static ResultSet runQuery(String query){
+    public static ResultSet runQuery(String query) {
 
         try {
-            Statement stmnt = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
-            rs =  stmnt.executeQuery(query) ;
+            Statement stmnt = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,
+                                                    ResultSet.CONCUR_READ_ONLY);
+            rs = stmnt.executeQuery(query);
 
         } catch (SQLException e) {
             e.printStackTrace();
         }
 
-        return  rs ;
+        return rs;
     }
 
-
-
-
-
-
-
 }
+
+
+
+
