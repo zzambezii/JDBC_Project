edc44da000d53915f6b44164f9c5382a0be578e8
Akbar <akbar@cybertekschool.com>
2020-08-04 14:20:14 -0400

    calling the utility methods

--- src/test/java/com/cybertek/jdbc/day2/DB_Practice2.java
+++ src/test/java/com/cybertek/jdbc/day2/DB_Practice2.java
@@ -15,33 +15,11 @@ public class DB_Practice2 {
         DB_Utility.displayAllData();
 
         // move resultset cursor to second row
-        rs.absolute(2);
-
+        // rs.absolute(2);
+        //DB_Utility.runQuery("SELECT * FROM EMPLOYEES WHERE SALARY>19000  ");
         DB_Utility.displayAllData();
 
-
-
-
-
-//        // get the first row data  | without knowing the column names
-//        int colCount = DB_Utility.getColumnCNT() ;
-//        // in order to get whole result cursor must be at before first location !
-//        while(rs.next() == true){ // row iteration
-//
-//            for (int i = 1; i <= colCount ; i++) { // column iteration
-//                System.out.print(  rs.getString( i )  + "\t" );
-//            }
-//            System.out.println(); /// adding a blank line for next line
-//
-//        }
-
-
-
-
-
-
-
-
+        System.out.println(DB_Utility.getColumnDataAtRow(3, 2));
 
 
     }
