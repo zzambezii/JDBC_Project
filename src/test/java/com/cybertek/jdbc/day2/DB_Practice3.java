0258266838e059f07f0e9823e3b7a64cb97924db
Akbar <akbar@cybertekschool.com>
2020-08-04 15:32:51 -0400

    Getting the row data as a map

---
+++ src/test/java/com/cybertek/jdbc/day2/DB_Practice3.java
@@ -0,0 +1,16 @@
+package com.cybertek.jdbc.day2;
+
+public class DB_Practice3 {
+
+    public static void main(String[] args) {
+
+
+        DB_Utility.createConnection();
+        DB_Utility.runQuery("SELECT * REGIONS") ;
+
+
+
+    }
+
+
+}
