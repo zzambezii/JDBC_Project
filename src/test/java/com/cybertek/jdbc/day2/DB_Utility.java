a9f21cebf734cf447af315ed74ef0a6817147e08
Akbar <akbar@cybertekschool.com>
2020-08-04 11:45:54 -0400

    Day 2 : DB_Utility.java getColumnCNT method added

--- src/test/java/com/cybertek/jdbc/day2/DB_Utility.java
+++ src/test/java/com/cybertek/jdbc/day2/DB_Utility.java
@@ -6,7 +6,52 @@ public class DB_Utility {
     // adding static field so we can access in all static methods
      private static Connection conn ;
      private static  ResultSet rs ;
+
+     /*
+     * a method to get the column count of the current ResultSet
+     *
+     *   getColumnCNT()
+     *
+     * */
+     public static int getColumnCNT(){
+
+         int colCount = 0  ;
+
+         try {
+             ResultSetMetaData rsmd = rs.getMetaData();
+             colCount = rsmd.getColumnCount() ;
+
+         } catch (SQLException throwables) {
+             System.out.println("ERROR WHILE COUNTING THE COLUMNS");
+             throwables.printStackTrace();
+         }
+
+        return colCount ;
+     }
+
+
+
     /*
+     * a static method to create connection
+     * with valid url and username password
+     * */
+    public static void createConnection() {
+
+        String connectionStr = "jdbc:oracle:thin:@52.71.242.164:1521:XE";
+        String username = "hr";
+        String password = "hr";
+
+        try {
+            conn = DriverManager.getConnection(connectionStr, username, password);
+            System.out.println("CONNECTION SUCCESSFUL");
+        } catch (SQLException throwables) {
+            System.out.println("CONNECTION HAS FAILED!");
+            throwables.printStackTrace();
+        }
+
+    }
+
+     /*
      * a static method to get the ResultSet object
      * with valid connection by executing query
      * */
@@ -23,25 +68,7 @@ public class DB_Utility {
         return  rs ;
     }
 
-    /*
-    * a static method to create connection
-    * with valid url and username password
-    * */
-    public static void createConnection() {
 
-        String connectionStr = "jdbc:oracle:thin:@52.71.242.164:1521:XE";
-        String username = "hr";
-        String password = "hr";
-
-        try {
-            conn = DriverManager.getConnection(connectionStr, username, password);
-            System.out.println("CONNECTION SUCCESSFUL");
-        } catch (SQLException throwables) {
-            System.out.println("CONNECTION HAS FAILED!");
-            throwables.printStackTrace();
-        }
-
-    }
 
 
 
