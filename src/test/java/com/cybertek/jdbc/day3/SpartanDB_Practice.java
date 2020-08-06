2f8ba63b08ccd9b92bb410a20b7f6188b70c92de
Akbar <akbar@cybertekschool.com>
2020-08-06 10:51:49 -0400

    JDBC Day 3 : Updating Utility to connect to different database

--- src/test/java/com/cybertek/jdbc/day3/SpartanDB_Practice.java
+++ src/test/java/com/cybertek/jdbc/day3/SpartanDB_Practice.java
@@ -1,5 +1,7 @@
 package com.cybertek.jdbc.day3;
 
+import com.cybertek.jdbc.utility.DB_Utility;
+
 public class SpartanDB_Practice {
 
     public static void main(String[] args) {
@@ -7,7 +9,12 @@ public class SpartanDB_Practice {
         // please create a package called utility
         // and move your DB_Utility under that package
 
-        String url = "";
+        String url = "jdbc:oracle:thin:@52.71.242.164:1521:XE";
+
+        DB_Utility.createConnection(url,"SP","SP");
+
+
+
 
 
 
