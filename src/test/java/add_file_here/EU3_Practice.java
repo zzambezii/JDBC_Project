93c96209c963025ff38ab1e5d03d703c7a5b54bf
EU3-User <fedbidtest1@gmail.com>
2020-10-07 15:46:32 -0400

    worked on us123 to add few more codes

--- src/test/java/add_file_here/EU3_Practice.java
+++ src/test/java/add_file_here/EU3_Practice.java
@@ -20,6 +20,7 @@ public class EU3_Practice {
         // we have added one new branch called us123
         // to work on user story 123 without affecting master
 
+        // adding more chnages here so I can have more history
     }
 
 }
