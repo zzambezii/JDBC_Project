bc3077ff3fb87d99cef9d966fd024815d0a13c76
EU3-User <fedbidtest1@gmail.com>
2020-10-07 16:29:58 -0400

    Avenger fan here?

---
+++ src/test/java/add_file_here/AvengerInfinity.java
@@ -0,0 +1,11 @@
+package add_file_here;
+
+public class AvengerInfinity {
+
+    public static void main(String[] args) {
+
+        // add four changes and commit with 4 separate commit messages
+
+    }
+
+}
