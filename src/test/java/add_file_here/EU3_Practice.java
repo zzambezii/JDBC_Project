7ef3723b2c4b47dc7b0e9962f459e41a2e036c42
EU3-User <fedbidtest1@gmail.com>
2020-10-07 12:46:47 -0400

    Updated git ignore and added sample file

---
+++ src/test/java/add_file_here/EU3_Practice.java
@@ -0,0 +1,19 @@
+package add_file_here;
+
+public class EU3_Practice {
+
+    public static void main(String[] args) {
+
+        System.out.println("EU3 is opening pull request");
+        // commit your change
+        //  with nice commit message
+        // push it back to the github
+        // observe the change
+        // from the your GitHub repo
+            // click on compare
+        // create a pull request (PR)
+             // to asl original repo owner
+             // to merge your change  
+    }
+
+}
