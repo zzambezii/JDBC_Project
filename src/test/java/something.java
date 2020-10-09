433265d5550cbf19434f394236c1a5069db418fb
zzambezii <zambezide@gmail.com>
2020-10-10 01:44:09 +0200

    someting new

--- src/test/java/something.java
+++ src/test/java/something.java
@@ -4,5 +4,10 @@ public class something {
 
     public static void main(String[] args) {
         System.out.println("i love you");
+
+
+        //again ai lvove you t
+
+        ///hatas another issue
     }
 }
